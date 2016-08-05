079edfdc96eeada27322a6ac3a2aec224b34ae4d
Travis Poulsen <travis.poulsen@gmail.com>
2016-08-05 07:47:27 -0400

    Add edges between vertices
    Edges represent the connection between two vertices. The `%Edge{}`
    struct is directed, consisting of two `%Vertex{}`s: `point`, and `next`,
    and the length between them.
    
    Length is calculated via Pythagorean theorem.
    
    For three edges, A -> B -> C, the angle b can be determined with
    `calculate_angle/3`. Edges are cast to vectors `AB` and `BC`; the angle
    `b` is the inverse cosine of the dot product of `AB` and `BC` over
    `||AB||` * `||BC||` (conveniently, these are the lengths of the edges).

--- lib/collision/polygon/vertex.ex
+++ lib/collision/polygon/vertex.ex
@@ -6,7 +6,7 @@ defmodule Collision.Polygon.Vertex do
   defstruct x: 0, y: 0
 
   @typedoc """
-  Vertices in two dimensional space are defied by `y` and `y` coordinates.
+  Vertices in two dimensional space are defined by `x` and `y` coordinates.
   """
   @type t :: t
 end
