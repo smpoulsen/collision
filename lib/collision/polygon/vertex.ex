564a1ea08fda166d87dc0ac6ea67aacd4275c807
Travis Poulsen <travis.poulsen@gmail.com>
2016-08-07 03:45:10 -0400

    Add Graham scan to construct convex hull
    Initial handling of concave polygons is by using the convex hull of the
    polygon's vertices.

--- lib/collision/polygon/vertex.ex
+++ lib/collision/polygon/vertex.ex
@@ -12,10 +12,95 @@ defmodule Collision.Polygon.Vertex do
   """
   @type t :: Vertex.t
 
+  @spec from_tuple({number, number}) :: Vertex.t
+  def from_tuple({x, y}) do
+    %Vertex{x: x, y: y}
+  end
+
   @spec to_tuple(Vertex.t) :: {number, number}
   def to_tuple(%Vertex{x: x, y: y}) do
     {x, y}
   end
+
+  @doc """
+  Determinant of the triangle formed by three vertices.
+
+  If > 0, counter-clockwise
+     = 0, colinear
+     < 0, clockwise
+  """
+  @spec determinant(Vertex.t, Vertex.t, Vertex.t) :: number
+  def determinant(%Vertex{} = v1, %Vertex{} = v2, %Vertex{} = v3) do
+    (v2.x - v1.x)*(v3.y - v1.y) - (v2.y - v1.y)*(v3.x - v1.x)
+  end
+  defp counter_clockwise(v1, v2, v3) do
+    determinant(v1, v2, v3) > 0
+  end
+
+  @doc """
+  Calculate the convex hull of a list of vertices.
+
+  In the case of a convex polygon, it returns the polygon's vertices.
+
+  For a concave polygon, it returns a subset of the vertices that form a
+  convex polygon.
+
+  ## Examples
+
+      iex> convex_polygon = [
+      ...>   %Vertex{x: 2, y: 2}, %Vertex{x: -2, y: 2},
+      ...>   %Vertex{x: -2, y: -2}, %Vertex{x: 2, y: -2}
+      ...> ]
+      ...> Vertex.graham_scan(convex_polygon)
+      [%Vertex{x: -2, y: -2}, %Vertex{x: 2, y: -2},
+       %Vertex{x: 2, y: 2}, %Vertex{x: -2, y: 2}]
+
+      iex> concave_polygon = [
+      ...>   %Vertex{x: 2, y: 2}, %Vertex{x: 0, y: 0}, %Vertex{x: -2, y: 2},
+      ...>   %Vertex{x: -2, y: -2}, %Vertex{x: 2, y: -2}
+      ...> ]
+      ...> Vertex.graham_scan(concave_polygon)
+      [%Vertex{x: -2, y: -2}, %Vertex{x: 2, y: -2},
+      %Vertex{x: 2, y: 2}, %Vertex{x: -2, y: 2}]
+
+  """
+  # Adapted from:
+  # https://gotorecursive.wordpress.com/2014/05/28/grahams-scan-from-imperative-to-functional/
+  @spec graham_scan([Vertex.t]) :: Vertex.t
+  def graham_scan(vertices) do
+    min_point = Enum.min_by(vertices, fn vertex -> [vertex.y, vertex.x] end)
+    distance_to_min = vertices
+    |> Enum.sort_by(fn vertex ->
+      :math.atan2(vertex.y - min_point.y, vertex.x - min_point.x)
+    end)
+
+    add_to_hull = fn point, hull ->
+      [point | filter_right_turns(point, hull)]
+    end
+
+    convex_hull = distance_to_min
+    |> List.foldl([], add_to_hull)
+    |> Enum.reverse
+    convex_hull
+  end
+  # During the scan, filter out the most recently added
+  # point if it makes a reflex angle with the new point.
+  defp filter_right_turns(new_point, hull) do
+    hull
+    |> List.foldr([], fn (current_point, current_hull) ->
+      case current_hull do
+        [previous | _] ->
+          if counter_clockwise(previous, current_point, new_point) do
+            [current_point | current_hull]
+          else
+            current_hull
+          end
+        _ ->
+          [current_point | current_hull]
+      end
+    end)
+  end
+
 
   @doc """
   Rounds the x and y components of a vertex.
